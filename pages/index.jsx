9270045bbc420a2c05a89c576c45a426fcb0535d
Wagner Paulo <wagstalos@hotmail.com>
2021-09-21 16:03:15 -0300

    porta #03

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,9 @@
-import Head from "next/head";
-import Presente from "../components/Presente";
+import Porta from "../components/Porta";
 
 export default function Home() {
   return (
     <div>
-      <Presente />
+      <Porta />
     </div>
   );
 }
